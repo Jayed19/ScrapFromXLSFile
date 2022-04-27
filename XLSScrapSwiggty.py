0718cb10f76e85173d1f99440c89d16dc4aa482b
Jayed <jayed19@gmail.com>
2022-04-28 00:20:06 +0600

    name finding half done

--- XLSScrapSwiggty.py
+++ XLSScrapSwiggty.py
@@ -43,22 +43,56 @@ def GenerateSQL():
     df = pd.read_excel(filedir,keep_default_na=False)
     
 
-    f = open("test1.txt", "a",encoding='utf-8')
+    f = open("test6.txt", "a",encoding='utf-8')
         #Row Finding
     for row in df.itertuples(index=False,name='eachrow'):
         #print(row)
         tuple1=row
         #print(tuple1)
-        ii = '[rowstart]'
+        ii = '[]'
 
 # Use for loop to convert tuple to string.
         for item in tuple1:
             it=item
-            ii = ii +'[join]'+str(it)
+            ii = ii +"[]"+str(it)
+        #ii="Hello Principal Coordinator (PC) POC: Test"
+        x=re.search(".*POC:", ii)
+        if x:
+            rowsting=x.string
+            splt=rowsting.split(",")
+            #print(splt)
+            for i in splt:
+                x1=re.search(".*POC:", i)
+                if x1 is None:
+                    splt.remove(i)
+                    
+            for i in splt:
+                if len(re.findall("POC:",i))>1:
+                    i=str(i).split("POC:")
+                    person1=i[1]
+                    person2=i[2]
+                    
+                    #person1=person1.replace("[]"," ")
+                    person1=re.sub("[][]+", "[]", person1)
+                    person1=person1.split("[]")
+                    person1=person1[1]
+                    #print("Person 1: "+person1[1])
 
-            
-        f.write(ii)
-        f.write("\n")
+                    
+
+                    
+                    person2=re.sub("[][]+", "[]", person2)
+                    person2=person2.split("[]")
+                    person2=person2[1]
+                    #print("Person 2: "+person2)
+
+                else:
+                    f.write(str(i))
+                f.write("\n")
+                
+                
+
+                    
     
     f.close()
 
